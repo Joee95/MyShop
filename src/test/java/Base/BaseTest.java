dcfeb26acfab3031109c048a9e27173fa10ab219
Khalid Magdi Mohammed <162809401+Kh-Magdi@users.noreply.github.com>
2024-06-23 20:24:57 +0300

    Enhancements

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -20,7 +20,7 @@ public class BaseTest extends AbstractTestNGCucumberTests {
     protected static LandingPage landingPage;
     protected static SignInPage signInPage;
     protected static RegistrationPage registrationPage;
-    protected static  HomePage homePage;
+    protected static HomePage homePage;
     protected static ProductPage productPage;
     protected static CartPage cartPage;
     protected static AddressPage addressPage;
@@ -32,31 +32,31 @@ public class BaseTest extends AbstractTestNGCucumberTests {
     protected static OrderHistoryPage orderHistoryPage;
 
     public static ChromeOptions chromeOption() {
-        options = new ChromeOptions();
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("profile.default.content_settings.popups", 0);
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
         chromePrefs.put("download.default_directory", downloadPath);
         options.setExperimentalOption("prefs", chromePrefs);
         options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        options.addArguments("--window-size=1024,768");
         return options;
     }
 
     @BeforeSuite
-    public void SetUpEnvironment() {
+    public void setUpEnvironment() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--window-size=1024,768");
+        options = chromeOption();
         driver = new ChromeDriver(options);
-        driver.navigate().to("http://www.automationpractice.pl/index.php");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
-        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("http://www.automationpractice.pl/index.php");
+
         landingPage = new LandingPage(driver);
     }
 
-
     @AfterSuite
-    public void CloseDriver (){
-        driver.quit();
+    public void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
 }
